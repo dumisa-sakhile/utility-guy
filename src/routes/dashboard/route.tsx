4a6610fe02609b13407a8149ef99b58c2d96b8a8
dumisa-sakhile <contact.dumisa@gmail.com>
2025-10-26 14:52:23 +0200

    add mobile padding

--- src/routes/dashboard/route.tsx
+++ src/routes/dashboard/route.tsx
@@ -423,7 +423,7 @@ function DashboardLayout() {
       </div>
 
       {/* Main Content */}
-      <div className="flex-1 flex flex-col min-w-0 overflow-hidden md:p-4">
+      <div className="flex-1 flex flex-col min-w-0 overflow-hidden p-2 md:p-4">
        
             <Outlet />
           
